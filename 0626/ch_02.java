10fe7f7051acec82f26d5005e160451512d02365
aww1111 <a0903532923@gmail.com>
2025-06-26 12:17:17 +0800

    Update ch_02.java

--- 0626/ch_02.java
+++ 0626/ch_02.java
@@ -1,3 +1,11 @@
+import java.util.Scanner;
+
 public class ch_02 {
-    
+    public static void main(String[] args) {
+        Scanner cin = new Scanner(System.in);
+
+        int a = cin.nextInt();
+        double b = a*a*3.14;
+        System.out.println(b);
+    }
 }
