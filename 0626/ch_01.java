88891736a021227713ea3b52135b7c4c4c5f38b3
aww1111 <a0903532923@gmail.com>
2025-06-26 12:07:39 +0800

    Update ch_01.java

---
+++ 0626/ch_01.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class ch_01 {
+    public static void main(String[] args) {
+        Scanner cin = new Scanner(System.in);
+        int a = cin.nextInt();
+        int b = cin.nextInt();
+
+        int c = a+b;
+        System.out.print(c);
+    }
+}
