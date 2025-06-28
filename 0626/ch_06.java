912ea923332e328b3c815ba4260ba21ca9e0d60c
aww1111 <a0903532923@gmail.com>
2025-06-29 05:18:17 +0800

    Update ch_06.java

--- 0626/ch_06.java
+++ 0626/ch_06.java
@@ -1,3 +1,24 @@
+import java.util.Random;
+
+
 public class ch_06 {
-    
+    public static void main(String[] args) {
+        int[] numbers = new int[10];
+        Random rand = new Random();
+
+        System.out.println("陣列中的隨機整數：");
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = rand.nextInt(100);
+            System.out.print(numbers[i] + " ");
+        }
+
+        int max = numbers[0];
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+        }
+
+        System.out.println("\n陣列中的最大值為：" + max);
+    }
 }
