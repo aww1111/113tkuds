4998a639d943df80f8ef9d2f5353ec62cdf52e9d
aww1111 <a0903532923@gmail.com>
2025-06-29 05:13:06 +0800

    Update ch_03.java

--- 0626/ch_03.java
+++ 0626/ch_03.java
@@ -3,5 +3,20 @@ public class ch_03 {
     public static void main(String[] args) {
         Scanner cin = new Scanner(System.in);
         
+        System.out.print("姓名：");
+        String name = cin.nextLine();
+
+        System.out.print("年齡：");
+        int age = cin.nextInt();
+        cin.nextLine();
+
+        System.out.print("城市：");
+        String city = cin.nextLine();
+
+        System.out.println("\n輸入的資料：");
+        System.out.println("姓名：" + name);
+        System.out.println("年齡：" + age);
+        System.out.println("城市：" + city);
+
     }
 }
