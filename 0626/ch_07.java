cd554ef18ce4d0a5f052d739fecf2252b528d07f
aww1111 <a0903532923@gmail.com>
2025-06-29 05:19:06 +0800

    Update ch_07.java

--- 0626/ch_07.java
+++ 0626/ch_07.java
@@ -1,3 +1,17 @@
 public class ch_07 {
-    
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        System.out.println("3x3 陣列中的元素為：");
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
 }
