18712c31ff518a6578f652652abe822d836cbdee
aww1111 <a0903532923@gmail.com>
2025-07-06 22:36:53 +0800

    Create q2seqsearch_20250706223135.java

---
+++ .history/q2seqsearch_20250706223135.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class q2seqsearch {
+    public static void main(String[] args){
+        Scanner cin = new Scanner(System.in);
+
+        int n = cin.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = cin.nextInt();
+        }
+        int key = cin.nextInt();
+
+        boolean found = false;
+        int ops = 0;
+
+        for (int i = 0; i < n; i++) {
+            ops++;
+            if (arr[i] == key) {
+                found = true;
+                break;
+            }
+        }
+        System.out.println(found ? "Yes" : "No");
+        System.out.println(ops);
+    }
+}
