d9bb656012dbf354b2bc13be972f2f526b2f98d0
aww1111 <a0903532923@gmail.com>
2025-07-15 12:35:16 +0800

    Create Q2_NextTHSRDeparture_20250715123504.java

---
+++ .history/Q2_NextTHSRDeparture_20250715123504.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Q2_NextTHSRDeparture {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        Scanner cin = new Scanner(System.in);
+        int n = cin.nextInt();
+        int[] times = new int[n];
+        for(int i=0;i<n;i++){
+            times[i] = parse(cin.nextLine());
+        }
+        int query = parse(cin.nextLine());
+    }
+}
+/*
+ * Time Complexity: O(…)
+ * 說明：…（推導 1~3 行）
+ */
