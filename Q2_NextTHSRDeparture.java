67579f159c23f28968722e9f91dfa97b39e75d73
aww1111 <a0903532923@gmail.com>
2025-07-15 12:35:21 +0800

    Update Q2_NextTHSRDeparture.java

--- Q2_NextTHSRDeparture.java
+++ Q2_NextTHSRDeparture.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
 public class Q2_NextTHSRDeparture {
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
-        
+        Scanner cin = new Scanner(System.in);
+        int n = cin.nextInt();
+        int[] times = new int[n];
+        for(int i=0;i<n;i++){
+            times[i] = parse(cin.nextLine());
+        }
+        int query = parse(cin.nextLine());
     }
 }
 /*
