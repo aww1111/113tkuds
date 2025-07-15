1264e2c5a769e996e8f1d0578e6303ab57399dd3
aww1111 <a0903532923@gmail.com>
2025-07-15 12:15:56 +0800

    Create Q1_THSRStopCounter_20250715121518.java

---
+++ .history/Q1_THSRStopCounter_20250715121518.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Q1_THSRStopCounter {
+    public static void main(String[] args) {
+        Scanner cin = new Scanner(System.in);
+        int n = cin.nextInt();
+
+        String[] station = new String[n];
+        for (int i=0;i<n;i++) {
+            station[i] = cin.next();
+        }
+
+        String start = cin.next();
+        String end = cin.next();
+        int startIdx = -1;
+        int endIdx = -1;
+
+        for (int i=0;i<n;i++) {
+            if(station[i].equals(start))startIdx=i;
+            if(station[i].equals(end))endIdx=i;
+        }
+        if(startIdx==-1||endIdx==-1){
+            System.out.println("Invalid");
+        }else{
+            int x = Math.abs(endIdx-startIdx)+1;
+            System.out.println(x);
+        }
+    }
+}
+/*
+ * Time Complexity: 4
+ * 說明：8~11行，建立輸入的站數陣列。
+ *      18~21行，尋找陣列中使用者輸入的起點站和終點站。
+ *      22~27行，如果頭尾其中一個沒找到，就印Invalid;都有的話就計算出站數印出。
+ */
